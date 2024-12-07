a34d564f18752c7024ee6b143960e984892bb4b7
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2024-12-07 16:21:02 +0100

    fix: adjust pathing in apps.py file to resolve deployment issue

--- articles/expert_lab1/expert_lab1/article_overview/apps.py
+++ articles/expert_lab1/expert_lab1/article_overview/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ArticleOverviewConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'article_overview'
+    name = 'articles.expert_lab1.expert_lab1.article_overview'
