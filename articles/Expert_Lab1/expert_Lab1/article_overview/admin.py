a8afcd9a089d4cfdbbdd79c49cfd077b6ae9cdda
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2024-12-07 15:34:24 +0100

    fix: change folder to temp folder to try fixing deployment error

--- articles/Expert_Lab1/expert_Lab1/article_overview/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Article
-
-# Register your models here.
-
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'date', 'source', 'image')
-
-admin.site.register(Article, ArticleAdmin)
