99d6e6d061dfc1d60c67935e1b1ed585f2e0202d
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2024-12-07 16:45:49 +0100

    build: adjust django settings module for deployment

--- articles/expert_lab1/expert_lab1/expert_lab1/wsgi.py
+++ articles/expert_lab1/expert_lab1/expert_lab1/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expert_lab1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expert_lab1.expert_lab1.expert_lab1.settings')
 
 application = get_wsgi_application()
